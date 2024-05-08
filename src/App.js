a87d53d1ac74dfe06d1a6d0dec3f12f7286ad921
Faizan <faizanh53@gmail.com>
2024-05-08 19:52:26 +1000

    Added auth flow

--- src/App.js
+++ src/App.js
@@ -1,41 +1,37 @@
 import './App.css';
-// import Home from "./components/home/home.js";
-
-import Cars from "./components/cars/cars.js";
-import Weightlifting from "./components/weightlifting/weightlifting.js";
-// import Industry from "./components/industry-experience/industry-experience.js";
-
-import Home from "./components/home/home_chakra.js";
-import Industry from "./components/industry-experience/chakra_industry.js";
-
-
-// import Blog from "./components/blog/chakra_blog.js";
-
-import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
-import theme from "./utils/theme.js";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React, { Component, useEffect, useState } from "react";
+import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
+import theme from "./utils/theme.js";
+
+import axios from "axios";
+import deployment from "./deployment";
+
+// Public
+import Layout from './components/Layout'
+import Login from "./features/auth/Login";
+// TODO import new login
+import Home from "./components/home/home_chakra.js";
+
+import Cars from "./components/cars/cars.js";
+import Weightlifting from "./components/weightlifting/weightlifting.js";
+import Industry from "./components/industry-experience/chakra_industry.js";
 
 import Blog from "./components/blog/chakra_blog.js";
 import CreateBlog from "./components/admin/admin_create_blog.js";
 import EditBlog from "./components/admin/admin_edit_blog.js";
 
-import axios from "axios";
-import deployment from "./deployment";
+// Protected
+import Admin from "./components/admin/admin_chakra.js";
+// import Prefetch from './features/auth/Prefetch'
+import PersistLogin from './features/auth/PersistLogin'
+import RequireAuth from './features/auth/RequireAuth'
+import { ROLES } from './utils/config/roles'
 
 // Misc Routes
 import ProtectedRoute from "./components/admin/protected-route";
 import Unauthorised from "./components/admin/unauthorised";
-
-// Protected
-import Admin from "./components/admin/admin_chakra.js";
-
-// Public
-import Login from "./components/admin/login.js";
-// import { AuthContextProvider } from './context/AuthContext';
-
-let _isMounted = false;
 
 let config = {
   withCredentials: true,
@@ -46,66 +42,35 @@ let config = {
   }
 };
 
-// Maybe put auth into its own component?
-
 function App() {
-  // const [user, setUser] = useState("");
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  // const setIsLoggedInState = user => {
-  //   setIsLoggedIn(true);
-  //   setUser(user);
-  // };
-  // const setLoggedOutState = user => {
-  //   setUser("");
-  //   setIsLoggedIn(false);
-  // };
-
-  // useEffect(() => {
-  //   _isMounted = true;
-
-  //   if (!isLoggedIn)
-  //     getAuth();
-
-  //   return () => { _isMounted = false };
-  // }, [isLoggedIn, user]);
-
-  // const getAuth = async () => {
-  //   if (_isMounted) {
-  //     const res = await axios.get(deployment.production + "/auth/user", config)
-  //       .catch(err => console.log(err));
-  //     if (res) {
-  //       if (res.data != "No User Found") {
-  //         setUser(res.data.user);
-  //         setIsLoggedIn(true);
-  //       } else {
-  //         setUser("");
-  //         setIsLoggedIn(false);
-  //       }
-  //     }
-  //   }
-  // }
-
   return (
     <ChakraProvider theme={extendTheme(theme)}>
       <ColorModeScript initialColorMode="light"></ColorModeScript>
       <div className="app-wrapper">
-        <BrowserRouter>
-          {/* <AuthContextProvider> */}
-            <Routes>
-              <Route path="/" element={<Home />} />
-              {/* <Route path="/login" element={<Login />} /> */}
-              <Route path="/_cpanel" element={<ProtectedRoute><Admin /></ProtectedRoute>} />
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />}/>
+              {/* public routes */}
               <Route path="/blog" element={<Blog mode={"all"} />} />
               <Route path="/new" element={<CreateBlog />} />
               <Route path="/edit/:id" element={<EditBlog />} />
               <Route path="/view/:id" element={<Blog mode={"single"} />} />
               <Route path="/motorsports" element={<Cars />} />
-              <Route path="/weightlifting" element={<Weightlifting />} />
+              {/* <Route path="/weightlifting" element={<Weightlifting />} /> */}
               <Route path="/industry" element={<Industry />} />
-            </Routes>
-          {/* </AuthContextProvider> */}
-        </BrowserRouter>
+              <Route path="/login" element={<Login />} />
+              {/* protected routes */}
+              <Route element={<PersistLogin />}>
+                <Route element={<RequireAuth allowedRoles={[...Object.values(ROLES)]} />}>
+                  {/* <Route element={<Prefetch />}> */}
+                    <Route element={<RequireAuth allowedRoles={[ROLES.Manager, ROLES.Admin]} />}>
+                      <Route path="/_cpanel" element={<Admin />} />
+                    </Route>
+                  {/* </Route> */}
+                </Route>
+              </Route>
+            </Route>
+          </Routes>
       </div>
     </ChakraProvider>
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux'
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { store } from './app/store'
 import reportWebVitals from './reportWebVitals';
 import $ from 'jquery';
 import { Popper } from '@popperjs/core';
@@ -14,7 +17,13 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
